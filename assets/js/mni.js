dc643c8682586d983e1a5cd44e4de2b878c622fc
NanosGoodman <153572017+nanosgoodman@users.noreply.github.com>
2024-04-22 16:02:32 -0400

    v1.0.3

--- assets/js/mni.js
+++ assets/js/mni.js
@@ -119,7 +119,7 @@ function get_address_data() {
     if (address == null) {
         return;
     }
-    RPC_SERVER = 'https://nanoslo.0x.no/proxy'; /*'https://node.somenano.com/proxy';*/
+    RPC_SERVER = 'https://nanoslo.0x.no/proxy';
     REQUEST_TIMEOUT = 10 * 1000;
     HISTORY_COUNT = 1000;
     TABLE_COUNT = 10;
